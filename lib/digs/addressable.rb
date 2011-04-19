00c495b2d87be8ae3518d8f39c90beabc04049f1
Phil McClure <pmcclure@rumblelabs.com>
2011-04-19 22:11:32 +0100

    Some spring cleaning in the addressable module

--- lib/digs/addressable.rb
+++ lib/digs/addressable.rb
@@ -1,13 +1,12 @@
 module Digs
   module Addressable
     extend ActiveSupport::Concern
-    ADDRESS_ATTRS = [:street, :town_or_city, :county, :postcode, :country, :additional]
 
     module ClassMethods
       def is_addressable
         self.has_one :address, :as => :addressable, :dependent => :destroy 
-        ADDRESS_ATTRS.each { |a| attr_accessor(a) }
-        #self.before_validation :create_address
+        accepts_nested_attributes_for :address
+        attr_accessor :street, :town_or_city, :county, :postcode, :country, :additional
       end
     end
   
@@ -15,26 +14,6 @@ module Digs
       unless self.address.valid?
         self.address.errors.each do |key,message|
           self.errors.add(key,message)
-        end
-      end
-    end
-
-    def collect_address_attrs
-      ADDRESS_ATTRS.inject({}) {|attrs, m| attrs[m] = send(m); attrs }
-    end
-
-    def create_address
-      if self.new_record?
-        self.address = Address.new(self.collect_address_attrs)
-      else
-        self.address.update_attributes(self.collect_address_attrs)
-      end
-    end
-
-    def after_initialize        
-      if self && self.address && !self.address.nil?
-        ADDRESS_ATTRS.each do |m|
-          self.send("#{m}=", self.address.send(m))
         end
       end
     end
